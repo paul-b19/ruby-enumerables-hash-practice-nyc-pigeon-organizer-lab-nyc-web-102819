08d05a6e25d31797cdaf558e4e9743b5cf1874cf
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-23 07:14:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
 def nyc_pigeon_organizer(data)
-  data.map{|key, value|
+  new_data = {}
+  data.map do |key, value|
+    value.map do |k, v|
+      new_data[v] = {}
+    end
+  end
+  p new_data
 end
