d91964a039993f28873fc1092756b4703831542a
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-23 08:30:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require 'pp'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  data.map do |key, value|
+    value.map do |k, v|
+      v.map{|i| new_data[i] = {}}
+    end
+  end
+  # pp new_data
+  
+  new_data.map do |key, value|
+    data.map do |k, v|
+      new_data[key][k] = []
+    end
+  end
+  # pp new_data
+  
+  data.map do |k, v|
+    v.map do |kk, vv|
+      vv.map do |i|
+        new_data[i][k] << kk.to_s
+      end
+    end
+  end
+  new_data
+  # pp new_data
 end
